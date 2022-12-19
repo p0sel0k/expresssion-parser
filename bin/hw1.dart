7d059e38481a8814f31834d874d3ff2bb5fe7aaf
Anton Poleshchuk <antonpoleshchuk56@gmail.com>
2022-12-19 20:50:20 +0500

    new algorithm implementation

--- bin/hw1.dart
+++ bin/hw1.dart
@@ -1,8 +1,9 @@
 import 'package:hw1/hw1.dart';
 
 void main() async {
-  var expr = "789*45+54-5";
+  var expr = "-567+x*(-y+35/(-43))";
 
-  var calculator = ExpressionParser(expression: expr, variables: {});
+  var calculator =
+      ExpressionParser(expression: expr, variables: {'x': 56, 'y': 34});
   print('Result of "$expr"  is: ${calculator.result()}');
 }
